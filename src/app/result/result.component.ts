9a698d35182ade35ea54ab4cdde5c17f6090cf27
HarshathM <harshathmdmaideen@gmail.com>
2022-10-31 14:01:26 +0530

    old files upload

---
+++ src/app/result/result.component.ts
@@ -0,0 +1,104 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { saveAs } from 'file-saver';
+import { CanvasJS } from 'src/assets/canvasjs.angular.component';
+
+// Done by Harshath.M
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.css']
+})
+export class ResultComponent implements OnInit {
+
+  constructor(private http:HttpClient) { }
+  csv_data:any;
+  metrics:any;
+	chartOptions:any;
+  keys:any;
+	load_graph = false;
+  load_table = false;
+  displayedColumns=["Metrics", "Values"];
+  api_url = "http://127.0.0.1:5000/api/";
+
+  ngOnInit(): void {
+		this.http.get(this.api_url+"get_csv",{responseType:"text"}).subscribe((res)=>{
+			this.csv_data = res;
+			this.open_page();
+			this.load_graph = true;
+		});
+    this.http.get(this.api_url+"metrics").subscribe({
+      next:((res:any)=>{
+        this.metrics=res;
+        this.keys = Object.keys(res);
+        this.load_table = true;
+      })
+    });
+
+  }
+  getDataPointsFromCSV(csv:any, col:number) {                       //https://canvasjs.com/docs/charts/how-to/create-charts-from-csv/
+    let dataPoints =[];
+		let csvLines = [];
+		let points = [];
+    let date = [];
+    csvLines = csv.split(/[\r?\n|\r|\n]+/);       
+    for (var i = 0; i < csvLines.length; i++)
+        if (csvLines[i].length > 0) {
+            points = csvLines[i].split(",");
+            date = points[0].split("-");
+            dataPoints.push({ 
+              x: new Date(date[0],date[1]-1),           //month starts from 0 . so only there is -1
+              y: parseInt(points[col]) 		
+				});
+		}
+		return dataPoints;
+	}
+	
+	open_page(){
+		this.chartOptions = {                              //https://canvasjs.com/angular-charts/multi-series-chart/
+      animationEnabled: true,
+			zoomEnabled: true,
+			exportEnabled: true,
+			theme: "light2",
+			title: {
+				text: "Predicted Graph"
+			},
+      axisX: {                                          //https://canvasjs.com/docs/charts/methods/canvasjs/format-date/
+        labelFormatter: function (e:any) {
+          return CanvasJS.formatDate( e.value, "MMM YY"); 
+        },
+        labelAngle: -20
+      }, 
+      axisY:{
+        title: "Sales"
+      },
+			data: [{
+				type: "line",
+        color:"orange",
+        showInLegend: true,
+		    name: "Actual Sales",
+		    dataPoints: this.getDataPointsFromCSV(this.csv_data, 1)
+			},{
+        type: "line",
+        color:"blue",
+        showInLegend: true,
+        name: "Predicted Sales",
+        dataPoints: this.getDataPointsFromCSV(this.csv_data, 2)
+      }]
+		}
+	}
+
+  download_csv(){
+    this.http.get(this.api_url+"get_csv",{responseType:"blob"}).subscribe((res)=>{
+			saveAs(res,"Predicted.csv");
+		});
+  }
+
+  download_metrics(){
+    this.http.get(this.api_url+"metrics",{responseType:"blob"}).subscribe((res)=>{
+			saveAs(res,"Metrics.json");
+		});
+  }
+
+}
---
+++ src/app/upload/upload.component.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+// Done by Harshath.M
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css']
+})
+export class UploadComponent implements OnInit {
+
+  file :any;
+  fname ='';
+  fformat='';
+  formdata:any;
+  periodicity= "6";
+  showbutton = true;
+
+  constructor(private snackbar:MatSnackBar,
+              private http:HttpClient,
+              private route:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  select_file(event : any){
+    try{
+      this.file = event.target.files[0];
+      if(this.file){
+        this.fname = this.file.name;
+        this.fformat = this.file.type;
+        if(this.fformat=='text/csv'){
+          this.formdata= new FormData();
+          this.formdata.append('dataset', this.file);
+        }
+        else{
+          this.snackbar.open("Please select a CSV file","Got it" ,{duration :3000});
+          this.deletefile();
+        }
+      }
+      console.log(this);    
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+  add_period(){
+    this.formdata.delete("period");
+    this.formdata.append('period', this.periodicity);
+  }
+  deletefile(){
+    this.fname='';
+    this.fformat='';
+    this.file=null;
+    this.formdata.delete("dataset");
+    console.log(this);
+  }
+
+// .subscribe(next?: ((value: string) => void) | null | undefined, 
+// error?: ((error: any) => void) | null | undefined,
+//  complete?: (() => void) | null | undefined): Subscription (+2 overloads)
+
+  predict(){
+      if(this.file){
+        this.showbutton=false;
+        this.add_period();
+        let api_url = "http://127.0.0.1:5000/api/predict";
+        this.http.post(api_url,this.formdata).subscribe({
+          next:((res:any)=>{
+            if(res.statuscode == 200){
+              this.snackbar.open("Eagar to see the prediction 😎","Go on",{duration:2500});  
+              this.route.navigate(['result']);
+            }
+            else{
+              this.snackbar.open(res.statusmessage,"Choose correct dataset",{duration:3000});
+              this.showbutton= true;
+            }
+          }),
+          error:(()=>{
+            this.showbutton=true;
+            this.snackbar.open("Oops! Server is not available 😟","Close", {duration:4000});
+          })
+        });
+      }
+      else{
+        this.snackbar.open("Please select a file 🙄","Okay",{duration:3000});
+      }
+  }
+}
